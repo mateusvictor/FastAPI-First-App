5b5d840624167af0862a0c3295426c628d46d930
Mateus Victor <mateus_victors@outlook.com>
2021-09-19 16:34:28 -0300

    add 'get_item()' function to CRUD's utils

--- sqlapp/crud.py
+++ sqlapp/crud.py
@@ -29,6 +29,9 @@ def get_items(db: Session, skip: int = 0, limit: int = 100):
 	return db.query(models.Item).offset(skip).limit(limit).all()
 
 
+def get_item(db: Session, item_id: int):
+	return db.query(models.Item).filter(models.Item.id == item_id).first()
+
 
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
 	db_item = models.Item(**item.dict(), owner_id=user_id)
@@ -36,5 +39,4 @@ def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
 	db.commit()
 	db.refresh(db_item)
 	return db_item
-
 
